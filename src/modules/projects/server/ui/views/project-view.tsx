9662550ecef2cdc74859f8d8e02d7789a910d90e
garvbarthwal <garvbarthwal@gmail.com>
2026-04-09 18:47:49 +0530

    fragment view

--- src/modules/projects/server/ui/views/project-view.tsx
+++ src/modules/projects/server/ui/views/project-view.tsx
@@ -9,6 +9,7 @@ import {
 import { MessagesContainer } from "../components/messages-container";
 import { Fragment } from "@/generated/prisma/client";
 import { ProjectHeader } from "../components/project-header";
+import { FragmentWeb } from "../components/fragment-web";
 
 interface Props {
     projectId: string;
@@ -40,7 +41,7 @@ export const ProjectView = ({ projectId }: Props) => {
                     defaultSize={65}
                     minSize={50}
                 >
-                    TODO: Preview
+                    {!!activeFragment && <FragmentWeb data={activeFragment}/>}
                 </ResizablePanel>
             </ResizablePanelGroup>
         </div>
