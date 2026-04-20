cc3e96f83049fd283cf8d07c185325f3e9014b8e
garvbarthwal <garvbarthwal@gmail.com>
2026-04-20 16:14:05 +0530

    bugs fixes and ui improvements

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -5,10 +5,10 @@ interface Props {
 const Layout = ( { children }: Props) => {
     return (
         <main className="flex flex-col min-h-screen max-h-screen">
-           <div className="absolute inset-0 -z-10 h-full w-full bg-background bg-gradient-to-b from-transparent to-black/40
+           <div className="absolute inset-0 -z-10 h-full w-full bg-background bg-linear-to-b from-transparent to-black/40
                 dark:bg-[radial-gradient(rgba(57,62,74,0.3)_1px,transparent_1px)]
                 bg-[radial-gradient(rgba(218,221,226,0.15)_1px,transparent_1px)]
-                [background-size:24px_24px]" 
+                bg-size-[24px_24px]" 
                 />
             <div className="flex-1 flex flex-col px-4 pb-4">
                 {children}
